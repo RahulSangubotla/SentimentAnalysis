5b98e41b33ce1e6e274db59bcef99725f04e42e4
Rahul Sangubotla <126463367+RahulSangubotla@users.noreply.github.com>
2024-03-26 23:14:16 +0530

    Add files via upload

---
+++ ui.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import re
+import string
+import numpy as np
+import shutil
+import tensorflow as tf
+from keras.models import load_model
+from keras import losses
+app = Flask(__name__)
+model=load_model('model_1.keras')
+@app.route('/')
+def student():
+   return render_template('home.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form['nm']
+      
+      ex=[
+         f"{result}"
+      ]
+      l=list(model.predict(ex))
+      x=0
+      for i in l:
+         x+=1
+         i=list(i)
+         maxpos = i.index(max(i))
+ 
+   return render_template("result.html",result=maxpos+1)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000,debug=True)
