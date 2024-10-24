40f8b703d5f23e39246a546f992443ae41fa93e9
Rahul Sangubotla <126463367+RahulSangubotla@users.noreply.github.com>
2024-10-24 13:20:39 +0530

    Add files via upload

--- ui.py
+++ ui.py
@@ -9,7 +9,7 @@ from keras import losses
 app = Flask(__name__)
 model=load_model('model_1.keras')
 @app.route('/')
-def student():
+def home():
    return render_template('home.html')
 
 @app.route('/result',methods = ['POST', 'GET'])
@@ -17,7 +17,7 @@ def result():
    if request.method == 'POST':
       result = request.form['nm']
       
-      ex=[
+      ex=[                                                              
          f"{result}"
       ]
       l=list(model.predict(ex))
@@ -27,7 +27,7 @@ def result():
          i=list(i)
          maxpos = i.index(max(i))
  
-   return render_template("result.html",result=maxpos+1)
+   return render_template("result.html",example=maxpos+1)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000,debug=True)
